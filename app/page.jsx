d7ce559fd3cfda83705793b552fa48676f3851f0
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-08-29 16:23:48 +0200

    edit: final tailwind custom configuration

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='container-full h-[50vh] perfect-center bg-base-300'>
+      <h1 className='font-h1 text-base-soft-content'>Hello World!</h1>
+    </div>
+  )
+}
