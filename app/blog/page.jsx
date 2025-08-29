1f10bdf0609b660d0e5ceb9e92dadeadeb11b587
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-08-29 18:29:22 +0200

    add: testing fetch

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,8 +1,19 @@
-export default function Blog() {
+export default async function Blog() {
+  // fetch lato server
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: "no-store" })
+  const users = await res.json()
+
   return (
     <div className='container-full h-[50vh] perfect-center bg-primary-soft'>
       <h1 className='font-h1 text-primary-soft-content'>Blog Home!</h1>
       <a href='/'>Torna alla home</a>
+      <ul className='list-disc'>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
