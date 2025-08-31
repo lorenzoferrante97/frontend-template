8e733bd572d904da9547ed5a3a861fbc2960e1b7
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-08-31 10:01:00 +0200

    add: testing local Image

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
+import Image from "next/image"
+import example from "@/assets/img/example.jpg"
+
 export default function Home() {
   return (
-    <div className='container-full h-[50vh] perfect-center bg-base-200'>
+    <div className='container-full h-[50vh] perfect-center gap-10 bg-base-200'>
       <h1 className='font-h1 text-base-soft-content'>Hello World!</h1>
-      <a href='/blog'>Vai al blog</a>
+      <figure className='h-52 aspect-[9/16] relative rounded-lg overflow-hidden'>
+        <Image
+          alt='immagine di esempio'
+          className='object-cover'
+          fill
+          placeholder='blur'
+          src={example}
+        />
+      </figure>
     </div>
   )
 }
