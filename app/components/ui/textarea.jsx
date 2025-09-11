dab25ad167876193a22f141430f2a68b9bb27bd3
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-09-11 09:23:39 +0200

    add: textarea

--- app/components/ui/textarea.jsx
+++ app/components/ui/textarea.jsx
@@ -1,20 +1,22 @@
-import * as React from "react"
+// import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Textarea({
-  className,
-  ...props
-}) {
+const textareaStyle =
+  "bg-base-100 border border-base-300 placeholder:bg-base-soft-content/75 min-h-16 w-full rounded-md px-3 py-2 font-body-base-normal text-base-soft-content selection:bg-custom-secondary-soft selection:text-custom-secondary-soft-content"
+
+function Textarea({ className, ...props }) {
   return (
     <textarea
-      data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-state aria-invalid:ring-custom-error aria-invalid:border-custom-error-soft flex field-sizing-content transition-colors outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        textareaStyle,
         className
       )}
-      {...props} />
-  );
+      data-slot='textarea'
+      {...props}
+    />
+  )
 }
 
 export { Textarea }
